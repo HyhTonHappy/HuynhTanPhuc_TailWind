e6c2314f58bdc32909b96153df2c0486742ae789
HyhTonHappy <tanphuchuynh2547@gmail.com>
2024-08-18 20:16:02 +0700

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], plugins: [require('flowbite/plugin')],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+
+  plugins: [
+    require('flowbite/plugin')
+],
+}
+
